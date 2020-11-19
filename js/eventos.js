6e3f758cd77fb96329d39e1aaadb3a53a15dd06e
johnsjohns <o-anao@hotmail.com>
2020-11-19 01:50:55 -0300

    feito logica de segurar botão e css

--- js/eventos.js
+++ js/eventos.js
@@ -1,3 +1,9 @@
-document.getElementById('botao').onclick = () => {som()};
-document.getElementById('mais').onclick = () => {mais()};
-document.getElementById('menos').onclick = () => {menos()};
+document.getElementById('botao').onclick = () => { som() };
+document.getElementById('mais').onmousedown = () => { aumentar() };
+document.getElementById('mais').onmouseup = () => { pararAumentar() };
+document.getElementById('mais').onmouseleave = () => { pararAumentar() };
+document.getElementById('menos').onmousedown = () => { diminuir() };
+document.getElementById('menos').onmouseup = () => { pararDiminuir() };
+document.getElementById('menos').onmouseleave  = () => { pararDiminuir() };
+
+document.getElementById('volume').onmousemove = () => { volume() };
--- js/js.js
+++ js/js.js
@@ -1,10 +1,14 @@
 
 var liga = true;
+var intervalId = 0;
 var BMP;
+var audio;
+var arquivo = './sons/Clicks/Metronome Hi.wav'
 var velocidade = document.getElementById('display').value;
+
 function som(){
     if(liga){
-    tocar(velocidade, './sons/Clicks/Metronome Hi.wav');
+    tocar(velocidade, arquivo );
     liga = false;
     } else {
         clearInterval(BMP);
@@ -15,7 +19,7 @@ function som(){
 
 
 function tocar(interval, som){
-    var audio = new Audio(som);
+    audio = new Audio(som);
     BMP = setInterval(() => {
         audio.play();
     }, ([60/interval*1000]));
@@ -23,8 +27,22 @@ function tocar(interval, som){
 
 function mais(){
     if(velocidade < 260){
-        velocidade = parseInt(velocidade) + 1; 
+        //aumenta em um a velociadade 
+        velocidade = parseInt(velocidade) + 1;
+        //atualizar o display para velocidade atual 
         document.getElementById('display').value = velocidade;
+    }
+}
+
+function aumentar() {
+    mais();
+    intervalId = setInterval(mais, 500);
+}
+
+function pararAumentar(){
+    if(intervalId !== 0){
+        clearInterval(intervalId);
+        intervalId = 0;
     }
 }
 
@@ -33,5 +51,28 @@ function menos(){
     if(velocidade > 40){
         velocidade = parseInt(velocidade) - 1; 
         document.getElementById('display').value = velocidade;
+    }
+}
+
+function diminuir(){
+    menos();
+    intervalId = setInterval(menos, 500);
+}
+
+function pararDiminuir(){
+    if(intervalId !== 0){
+        clearInterval(intervalId);
+        intervalId = 0;
+    }
+}
+
+function volume(){
+    // armazenando o valor atual da slide bar
+    intensidade = document.getElementById('volume').value;
+
+    // verifica primeiro se o audio já existe
+    if(audio !== undefined){
+        //volume varia de 1 a 0. para ajusta o som a 20% fazemos audio.volume = 0,2. como a slide bar vai de 1 a 100, dividimos esse numero por 100
+        audio.volume = intensidade / 100;
     }
 }
